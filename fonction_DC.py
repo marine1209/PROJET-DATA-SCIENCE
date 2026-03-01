1cd8f4ce4485692f327707044a82a57533143773
marine anglade <marine.anglade@epfedu.fr>
2026-03-01 18:25:07 +0100

    fonction clean_data_temperature

--- fonction_DC.py
+++ fonction_DC.py
@@ -1,10 +1,36 @@
 # Ajouter les fonction de data cleaning dans cette feuille 
 
 #Bibliothèques 
-
+import pandas as pd 
+import numpy as np 
 
 # Ana : Production ENR et/ou Nucléaire 
 
 # Marie : Prix de l'énergie sur le marché SPOT
 
 # Marine : Température & pseudo rayonnement
+
+def clean_data_temperature (chemin_fichier): 
+    fichier = pd.read_csv(chemin_fichier, sep=";")
+    fichier["Horodate"] = pd.to_datetime(fichier["Horodate"], utc = True)
+    fichier = fichier.drop(columns=["Année-Mois-Jour"])
+    fichier = fichier.drop(columns = ["Année"])
+    fichier = fichier.drop(columns = ["Mois"])
+    fichier = fichier.drop(columns = ["Jour"])
+    print(fichier.isna().sum())
+    print(fichier.isnull().sum() )
+    print(fichier.duplicated().sum())
+    print(fichier.info())
+    print(fichier.head())
+    print(fichier.describe()) 
+    return fichier
+    
+    
+#def traitement_data_temperature(fichier_nettoye): 
+    
+
+
+
+
+
+
