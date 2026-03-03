154aa588c93ca9b384c8767ccdc61565f9ef36e5
marie bertrand <marie.bertrand@epfedu.fr>
2026-03-03 09:36:22 +0100

    traitement data prix

--- fonction_DC.py
+++ fonction_DC.py
@@ -7,7 +7,22 @@ import numpy as np
 # Ana : Production ENR et/ou Nucléaire 
 
 # Marie : Prix de l'énergie sur le marché SPOT
-
+def clean_data_price (chemin_fichier):
+    df = pd.read_csv(chemin_fichier)
+    if df["BZN|FR"].isna().all(): # on vérifie que la colonne est vide avant de la supprimer
+        df = df.drop(columns=["BZN|FR"])
+        print('La colonne "BZN|FR" était vide et a été supprimée.')
+    else:
+        print('La colonne "BZN|FR" contient des données.')
+    df=df.drop(columns=["Currency"])
+    df[["Date_start", "Date_end"]] = df["MTU (CET/CEST)"].str.split(" - ", expand=True) 
+    df = df.drop(columns=["MTU (CET/CEST)"]) # on supprime l'ancienne colonne de la plage horaire
+    df["Date_start"] = pd.to_datetime(df["Date_start"], format="%d.%m.%Y %H:%M", utc = True).dt.tz_localize(None) # on change de le format de la date de début et de fin
+    df["Date_end"] = pd.to_datetime(df["Date_end"], format="%d.%m.%Y %H:%M", utc = True).dt.tz_localize(None) # on change de le format de la date de fin
+    print(df.shape)
+    print(df.head())
+    return df
+#clean_data_price(".\Data\Day-ahead Prices_2019-2020.csv")
 # Marine : Température & pseudo rayonnement
 
 def clean_data_temperature (chemin_fichier): 
